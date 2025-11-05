6c68b3e549d36069c50f22ea129e91c31c9d131a
vikashsiyak <vikashsiyak7014@gmail.com>
2025-11-05 22:09:44 +0530

    Add sandbox functionality to process backtest results
    Create a sandbox copy of backtest results with noise.

--- src/sandbox.py
+++ src/sandbox.py
@@ -1,1 +1,7 @@
+import pandas as pd, numpy as np
+
+bt = pd.read_csv('data/backtest_results.csv', index_col=0)
+sandbox = bt.copy()
+sandbox['live_pnl'] = bt.iloc[:, 0] + np.random.normal(0, 2, len(bt))  # +/- 2 noise
+sandbox.to_csv('data/sandbox_results.csv')
 
